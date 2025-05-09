9a2484455454e556f78b0fd293330d004f430dbf
fedepini <fpini95@gmail.com>
2025-05-09 14:46:40 +0200

    feat: updated routes

--- src/widgets/Route/Route.tsx
+++ src/widgets/Route/Route.tsx
@@ -3,9 +3,16 @@ import { WidgetRenderer } from '../../components/WidgetRenderer'
 import type { WidgetProps } from '../../types/Widget'
 import { getEndpointUrl } from '../../utils/utils'
 
-export function Route(
-  props: WidgetProps<{ resourceRefId: string }>,
-) {
+import styles from './Route.module.css'
+
+export function Route(props: WidgetProps<{ items: { resourceRefId: string }[] }>) {
   const { widgetData, resourcesRefs } = props
-  return <WidgetRenderer widgetEndpoint={getEndpointUrl(widgetData.resourceRefId, resourcesRefs)} />
+
+  return (
+    <div className={styles.route}>
+      {widgetData.items.map((item) => {
+        return <WidgetRenderer widgetEndpoint={getEndpointUrl(item.resourceRefId, resourcesRefs)} />
+      })}
+    </div>
+  )
 }
