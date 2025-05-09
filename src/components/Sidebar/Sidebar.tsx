c2853cc185439ed44db54b8dfb6ae5b31d0355ab
fedepini <fpini95@gmail.com>
2025-05-09 11:01:31 +0200

    feat: updated css for sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,4 +1,3 @@
-import { Menu } from 'antd'
 
 import logo from '../../assets/images/logo.png'
 import { getResourceEndpoint } from '../../utils/utils'
@@ -11,16 +10,14 @@ const Sidebar = () => {
     <div className={styles.sidebar}>
       <img alt='Krateo DevOpsApp' className={styles.logo} src={logo} />
 
-      <Menu>
-        <WidgetRenderer
-          widgetEndpoint={getResourceEndpoint({
-            name: 'sidebar-nav-menu',
-            namespace: 'krateo-system',
-            resource: 'navmenus',
-            version: 'v1beta1',
-          })}
-        />
-      </Menu>
+      <WidgetRenderer
+        widgetEndpoint={getResourceEndpoint({
+          name: 'sidebar-nav-menu',
+          namespace: 'krateo-system',
+          resource: 'navmenus',
+          version: 'v1beta1',
+        })}
+      />
     </div>
   )
 }
--- src/widgets/NavMenu/NavMenu.tsx
+++ src/widgets/NavMenu/NavMenu.tsx
@@ -1,9 +1,13 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
+import type { IconProp } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Menu } from 'antd'
 import type { MenuItemType } from 'antd/es/menu/interface'
 import { useNavigate } from 'react-router'
 
 import type { WidgetProps } from '../../types/Widget'
+
+import styles from './NavMenu.module.css'
 
 export function NavMenu(
   props: WidgetProps<{
@@ -17,11 +21,18 @@ export function NavMenu(
   const navigate = useNavigate()
 
   const { items } = props.widgetData
-  const menuItems: MenuItemType[] = items.map(({ icon, label, resourceRefId }) => ({ icon, key: resourceRefId, label }))
+
+  const menuItems: MenuItemType[] = items.map(({ icon, label, resourceRefId }) => ({
+    icon: <FontAwesomeIcon icon={icon as IconProp} />,
+    key: resourceRefId,
+    label,
+  }))
 
   return (
     <Menu
+      className={styles.menu}
       items={menuItems}
+      mode='inline'
       onClick={item => navigate(item.key)}
     />
   )
