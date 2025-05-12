6a2a0a23d1dca64a3ad8aeeaf996fd87bbf50995
fedepini <fpini95@gmail.com>
2025-05-12 14:26:34 +0200

    feat: added Breadcrumb component

---
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -0,0 +1,48 @@
+import { Breadcrumb as AntBreadcrumb } from 'antd'
+import type { BreadcrumbItemType, BreadcrumbSeparatorType } from 'antd/es/breadcrumb/Breadcrumb'
+import { useEffect, useState } from 'react'
+import { Link, useMatches } from 'react-router'
+
+import styles from './Breadcrumb.module.css'
+
+const getFullPath = (index: number, splitPath: string[]) => {
+  const url: string[] = []
+
+  splitPath.forEach((element, splitIndex) => {
+    if (splitIndex <= index) {
+      url.push(element)
+    }
+  })
+
+  return url.join('/')
+}
+
+const Breadcrumb = () => {
+  const [items, setItems] = useState<Partial<BreadcrumbItemType & BreadcrumbSeparatorType>[]>()
+  const matches = useMatches()
+
+  useEffect(() => {
+    const path = matches.filter(({ pathname }) => pathname !== '/')[0]?.pathname?.replace('/', '')
+
+    if (path) {
+      const items: Partial<BreadcrumbItemType & BreadcrumbSeparatorType>[] = []
+      const splitPath = path.split('/')
+
+      splitPath.forEach((pathElement, index) => {
+        if (index === splitPath.length - 1) {
+          items.push({ title: <span className={styles.breadcrumbItem}>{pathElement}</span> })
+        } else {
+          items.push({ title: <Link to={getFullPath(index, splitPath)}>{pathElement}</Link> })
+        }
+      })
+
+      setItems(items)
+    } else {
+      setItems([{ title: '' }])
+    }
+  }, [matches])
+
+  return <AntBreadcrumb items={items}/>
+}
+
+export default Breadcrumb
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import { Header as AntHeader } from 'antd/es/layout/layout'
 
+import Breadcrumb from '../Breadcrumb'
+
 import styles from './Header.module.css'
 
 const Header = () => {
   return (
     <AntHeader className={styles.header}>
       <div className={styles.content}>
+        <Breadcrumb />
       </div>
     </AntHeader>
   )
