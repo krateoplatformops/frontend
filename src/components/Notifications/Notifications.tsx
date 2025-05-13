fe9ae3f5aa0db48748d1c87fa357ff03715b8714
Jaga Santagostino <jagasantagostino@gmail.com>
2025-05-13 21:59:56 +0200

    add notifications/events

--- src/components/Notifications/Notifications.tsx
+++ src/components/Notifications/Notifications.tsx
@@ -1,6 +1,9 @@
 import { BellFilled } from '@ant-design/icons'
+import { useQuery } from '@tanstack/react-query'
 import { Badge, Button, Drawer, Skeleton } from 'antd'
 import { useMemo, useState } from 'react'
+
+import { useConfigContext } from '../../context/ConfigContext'
 
 import styles from './Notifications.module.css'
 
@@ -8,44 +11,73 @@ import styles from './Notifications.module.css'
 // understand if Redux is still needed
 // render notifications
 
+export type NotificationType = {
+  id: string
+  title: string
+  description: string
+  createdAt: string
+}
+
 const Notifications = () => {
   const [drawerVisible, setDrawerVisible] = useState(false)
 
+  const { config } = useConfigContext()
+  const eventsApiUrl = `${config!.api.EVENTS_API_BASE_URL}/events`
+
+  const { data: notifications, isLoading } = useQuery({
+    enabled: drawerVisible,
+    queryFn: async () => {
+      const res = await fetch(eventsApiUrl)
+      const notificasions = (await res.json()) as NotificationType[]
+      return notificasions.reverse()
+    },
+    queryKey: ['notifications', eventsApiUrl],
+  })
+
   const count = 0
 
-  const button = useMemo(() => (
-    <Badge
-      className={`${styles.badge} ${count > 0 ? styles.hasNotifications : ''}`}
-      count={count}
-      //  count={notifications.length}
-    >
-      <Button
-        className={styles.icon}
-        icon={<BellFilled />}
-        onClick={() => setDrawerVisible(true)}
-        shape='circle'
-        type='link'
-      />
-    </Badge>
-  ), [count])
+  const button = useMemo(
+    () => (
+      <Badge
+        className={`${styles.badge} ${count > 0 ? styles.hasNotifications : ''}`}
+        count={count}
+        //  count={notifications.length}
+      >
+        <Button
+          className={styles.icon}
+          icon={<BellFilled />}
+          onClick={() => setDrawerVisible(true)}
+          shape="circle"
+          type="link"
+        />
+      </Badge>
+    ),
+    [count],
+  )
 
-  const drawer = useMemo(() => (
+  const drawer = (
     <Drawer
       onClose={() => setDrawerVisible(false)}
       open={drawerVisible}
-      title='Notifications'
+      title="Notifications"
       width={550}
     >
-      <Skeleton />
+      {isLoading && <Skeleton active />}
+      {notifications?.map((notification) => (
+        <div key={notification.metadata.uid} style={{ marginBottom: 24 }}>
+          <h3>{notification.metadata.name}</h3>
+          <p>{notification.metadata.uid}</p>
+          <p>{notification.message}</p>
+        </div>
+      ))}
     </Drawer>
-  ), [drawerVisible])
+  )
 
   return (
     <>
       {button}
       {drawer}
     </>
-
   )
 }
 
