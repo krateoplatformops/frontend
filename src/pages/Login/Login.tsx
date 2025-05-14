01df377de3fc48d84213a6b76e7ca5686b2b0132
fedepini <fpini95@gmail.com>
2025-05-14 13:15:16 +0200

    feat: updated Login to introduce style and cleaned code

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,8 +1,12 @@
 import { useMutation, useQuery } from '@tanstack/react-query'
+import { Card, Divider, Result, Skeleton } from 'antd'
+import { useCallback, useMemo } from 'react'
 import { useNavigate } from 'react-router'
 
+import logo from '../../assets/images/logo_big.svg'
 import { useConfigContext } from '../../context/ConfigContext'
 
+import styles from './Login.module.css'
 import LoginForm from './LoginForm'
 import type { AuthModeType, FormType, LoginFormType } from './types'
 
@@ -12,18 +16,18 @@ const Login = () => {
   const authUrl = `${config!.api.AUTHN_API_BASE_URL}/strategies`
 
   const {
-    data,
-    // error,
+    data: methods,
+    error: isMethodsError,
+    isLoading: isMethodLoading,
   } = useQuery({
     queryFn: async () => {
       const res = await fetch(authUrl)
-      const data = await res.json() as AuthModeType[]
-
-      return data
+      return await res.json() as AuthModeType[]
     },
-    queryKey: ['credentials', authUrl],
+    queryKey: ['methods', authUrl],
   })
 
+  // TODO: handle error
   const {
     isPending: isLoginLoading,
     mutateAsync: login,
@@ -39,29 +43,84 @@ const Login = () => {
         method: 'GET',
       })
 
-      const data = await res.json() as AuthModeType[]
-
-      return data
+      return await res.json() as AuthModeType[]
     },
   })
 
-  if (!data) {
-    return <></>
-  }
+  const onFormSubmit = useCallback(async (body: LoginFormType, type: FormType) => {
+    const { username, password } = body
+    const method = methods?.find(({ kind }) => kind === type)
 
-  const onFormSubmit = async (body: LoginFormType, type: FormType) => {
-    const dataMode = data?.find((el) => el.kind === type)
+    if (username && password && method?.path) {
+      try {
+        await login({ password, path: method.path, username })
+          .then(() => navigate('/'))
+      } catch (error) {
+        // TODO: handle error
+        console.error('Login failed:', error)
+      }
+    }
+  }, [login, methods, navigate])
 
-    if (body.username && body.password && dataMode?.path) {
-      const loginData = await login({ password: body.password, path: dataMode.path, username: body.username })
-      console.log('loginData', loginData)
+  const content = useMemo(() => {
+    if (isMethodLoading) {
+      return <Skeleton />
     }
 
-    void navigate('/')
-  }
+    if (isMethodsError) {
+      return <Result status='error' subTitle='Unable to retrieve authentication methods' title="Ops! Something didn't work" />
+    }
 
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
-  return <LoginForm isLoading={isLoginLoading} method={data?.[0]} onSubmit={(values) => onFormSubmit(values, data[0].kind as FormType)} />
+    if (methods) {
+      if (methods.length === 0) {
+        return (
+          <Result
+            status='warning'
+            subTitle='Please create some authentication methods and try again'
+            title='There are no authentication methods'
+          />
+        )
+      }
+
+      return methods.map((method, index) => {
+        const { kind } = method
+
+        if (kind === 'basic' || kind === 'ldap') {
+          return (
+            <div key={`login_${index}`}>
+              <LoginForm
+                isLoading={isLoginLoading}
+                method={method}
+                onSubmit={(values) => { void onFormSubmit(values, kind) }}
+              />
+              {((index + 1) < methods?.length) && <Divider plain>OR</Divider> }
+            </div>
+          )
+        }
+
+        // TODO: add social login
+        return <></>
+      })
+    }
+  }, [isMethodLoading, isMethodsError, methods, isLoginLoading, onFormSubmit])
+
+  return (
+    <div className={styles.login}>
+      <aside className={styles.aside}>
+        <img alt='Krateo | PlatformOps' className={styles.image} src={logo} />
+      </aside>
+      <section className={styles.section}>
+        <Card
+          className={styles.card}
+          classNames={{ body: styles.body, header: styles.header, title: styles.title }}
+          title={<span className={styles.title}>Welcome back</span>}
+          variant={'borderless'}
+        >
+          {content}
+        </Card>
+      </section>
+    </div>
+  )
 }
 
 export default Login
--- src/pages/Login/LoginForm/LoginForm.tsx
+++ src/pages/Login/LoginForm/LoginForm.tsx
@@ -1,9 +1,9 @@
 import { Button, Form, Input } from 'antd'
 import { Link } from 'react-router'
 
+import type { AuthModeType, LoginFormType } from '../types'
 
 import styles from './LoginForm.module.css'
-import type { AuthModeType, LoginFormType } from './types'
 
 type LoginType = {
   method: AuthModeType
@@ -13,7 +13,7 @@ type LoginType = {
 
 const LoginForm = ({ method, onSubmit, isLoading }: LoginType) => {
   return (
-    <section className={styles.loginForm}>
+    <section className={styles.form}>
       <Form
         autoComplete='off'
         disabled={isLoading}
@@ -38,25 +38,32 @@ const LoginForm = ({ method, onSubmit, isLoading }: LoginType) => {
           <Input.Password size='large' />
         </Form.Item>
 
-        { method.kind === 'basic'
-          && <div className={styles.linkPassword}>
+        { method.kind === 'basic' && (
+          <div className={styles.link}>
             <Link to='/forgotpassword'>Forgot password?</Link>
           </div>
-        }
+        )}
 
         <Form.Item>
           { method.kind === 'basic'
-            ? <Button className={styles.loginButton} htmlType='submit' size='large' type='primary'>
-              Sign In
-            </Button>
-            : <Button
-              className={styles.loginButton}
-              htmlType='submit'
-              size='large'
-              style={method.graphics?.backgroundColor && method.graphics?.textColor ? { backgroundColor: method.graphics.backgroundColor, color: method.graphics.textColor } : undefined}
-            >
-              {method.graphics?.displayName || 'LDAP Sign In'}
-            </Button>
+            ? (
+              <Button className={styles.button} htmlType='submit' size='large' type='primary'>
+                Sign In
+              </Button>
+            ) : (
+              <Button
+                className={styles.button}
+                htmlType='submit'
+                size='large'
+                style={
+                  method.graphics?.backgroundColor && method.graphics?.textColor
+                    ? { backgroundColor: method.graphics.backgroundColor, color: method.graphics.textColor }
+                    : undefined
+                }
+              >
+                {method.graphics?.displayName || 'LDAP Sign In'}
+              </Button>
+            )
           }
         </Form.Item>
       </Form>
