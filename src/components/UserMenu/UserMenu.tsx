7a36c13bdc8f4a581ce030accb42c451900d3818
fedepini <fpini95@gmail.com>
2025-05-14 17:57:44 +0200

    feat; updated UserMenu style and added logout

--- src/components/UserMenu/UserMenu.tsx
+++ src/components/UserMenu/UserMenu.tsx
@@ -1,15 +1,14 @@
 import type { MenuProps } from 'antd'
-import { Avatar, Flex, Menu, Popover, Typography } from 'antd'
-import { noop } from 'lodash'
-import { Link } from 'react-router'
+import { Avatar, Menu, Popover, Typography } from 'antd'
+import { Link, useNavigate } from 'react-router'
 
 import type { AuthResponseType } from '../../pages/Login/Login.types'
 
 import styles from './UserMenu.module.css'
 
-// TODO: add style
-
 const UserMenu = () => {
+  const navigate = useNavigate()
+
   const userData = JSON.parse(localStorage.getItem('K_user') || '{}') as AuthResponseType
   const { avatarURL, displayName, username } = userData.user || {}
 
@@ -28,7 +27,10 @@ const UserMenu = () => {
   const groups = ['devOps', 'managers']
 
   // TODO: handle logout
-  const onLogout = () => noop
+  const onLogout = () => {
+    localStorage.removeItem('K_user')
+    void navigate('/login')
+  }
 
   const items: MenuProps['items'] = [
     {
@@ -41,29 +43,30 @@ const UserMenu = () => {
   return (
     <Popover
       arrow={false}
-      className={styles.userLogged}
+      className={styles.popover}
       content={
         <section className={styles.panel}>
-          <Flex align='center' className={styles.userData} gap={20} vertical>
+          <div className={styles.userData}>
             <Avatar
               gap={2}
               size={80}
               src={avatarURL}
             >
-              <Typography.Text className={styles.sign}>{initials}</Typography.Text>
+              <Typography.Text className={styles.initials}>{initials}</Typography.Text>
             </Avatar>
 
-            <Flex align='center' className={styles.details} gap={3} vertical>
+            <div className={styles.details}>
               <Typography.Text className={styles.fullname}>{fullName}</Typography.Text>
               <Typography.Text className={styles.role}>{role}</Typography.Text>
               <Typography.Text className={styles.groups}>{groups.join(', ')}</Typography.Text>
-            </Flex>
-          </Flex>
+            </div>
+          </div>
 
-          <Menu items={items}
+          <Menu
+            className={styles.menu}
+            items={items}
             mode='vertical'
             selectable={false}
-            style={{ border: 'none' }}
           />
         </section>
       }
