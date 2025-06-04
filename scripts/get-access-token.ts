ce67eaab609ed179c7036ae788adeeae53e975fe
Jaga Santagostino <jagasantagostino@gmail.com>
2025-06-04 11:51:16 +0200

    read credentials from ENV

--- scripts/get-access-token.ts
+++ scripts/get-access-token.ts
@@ -1,16 +1,16 @@
-// curl 'http://20.166.174.117:8082/basic/login' \
-//   -H 'authorization: Basic YWRtaW46TlFwTFNicXRjRkdE' \
-//   --insecure
-
-// echo -n 'admin:NQpLSbqtcFGD' | base64
-
-const adminUsername = 'admin'
-const adminPassword = 'zzXeMF8aTitj'
-
 import { writeFileSync } from 'node:fs'
 import { join } from 'node:path'
 
 import { api } from '../public/config/config.json'
+
+const adminUsername = process.env.ADMIN_USERNAME
+if (!adminUsername) {
+  throw new Error('ADMIN_USERNAME env is not set')
+}
+const adminPassword = process.env.ADMIN_PASSWORD
+if (!adminPassword) {
+  throw new Error('ADMIN_PASSWORD env is not set')
+}
 
 export async function getAccessToken() {
   const credentials = Buffer.from(`${adminUsername}:${adminPassword}`).toString('base64')
@@ -20,7 +20,11 @@ export async function getAccessToken() {
       Authorization: `Basic ${credentials}`,
       'Content-Type': 'application/json',
     },
-  })
+  }) // curl 'http://20.166.174.117:8082/basic/login' \
+  //   -H 'authorization: Basic YWRtaW46TlFwTFNicXRjRkdE' \
+  //   --insecure
+
+  // echo -n 'admin:NQpLSbqtcFGD' | base64
 
   const data = await response.json()
   writeFileSync(join(process.cwd(), 'scripts', 'login-response.json'), JSON.stringify(data, null, 2))
