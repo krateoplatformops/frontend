0cd52e7c91e79f64411f5c05d86940c4ebcfbe80
fedepini <fpini95@gmail.com>
2025-06-10 17:02:09 +0200

    feat: updated column spacing in Row

--- src/widgets/Row/Row.tsx
+++ src/widgets/Row/Row.tsx
@@ -4,6 +4,7 @@ import WidgetRenderer from '../../components/WidgetRenderer'
 import type { WidgetProps } from '../../types/Widget'
 import { getEndpointUrl } from '../../utils/utils'
 
+import styles from './Row.module.css'
 import type { Row as WidgetType } from './Row.type'
 
 export type RowWidgetData = WidgetType['spec']['widgetData']
@@ -16,7 +17,7 @@ const Row = ({ resourcesRefs, uid, widgetData }: WidgetProps<RowWidgetData>) => 
   return (
     <AntdRow align={'middle'} gutter={{ lg: 32, md: 24, sm: 16, xs: 8 }} justify={'center'} key={uid} wrap>
       {items.map(({ resourceRefId, size }, index) => (
-        <AntdColumn key={`${uid}-col-${index}`} span={size ?? defaultSize}>
+        <AntdColumn className={styles.column} key={`${uid}-col-${index}`} span={size ?? defaultSize}>
           <WidgetRenderer key={`${uid}-${index}`} widgetEndpoint={getEndpointUrl(resourceRefId, resourcesRefs)} />
         </AntdColumn>
       ))}
