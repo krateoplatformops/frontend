eb34bf4862cb46be17a13ae0d0b51e57503e8ca1
fedepini <fpini95@gmail.com>
2025-06-19 11:19:30 +0200

    fix: fixed gap in Column items

--- src/widgets/Column/Column.tsx
+++ src/widgets/Column/Column.tsx
@@ -4,6 +4,7 @@ import WidgetRenderer from '../../components/WidgetRenderer'
 import type { WidgetProps } from '../../types/Widget'
 import { getEndpointUrl } from '../../utils/utils'
 
+import styles from './Column.module.css'
 import type { Column as WidgetType } from './Column.type'
 
 export type ColumnWidgetData = WidgetType['spec']['widgetData']
@@ -12,7 +13,7 @@ const Column = ({ resourcesRefs, uid, widgetData }: WidgetProps<ColumnWidgetData
   const { items, size } = widgetData
 
   return (
-    <AntdColumn key={uid} span={size}>
+    <AntdColumn className={styles.column} key={uid} span={size}>
       {items.map(({ resourceRefId }, index) => (
         <WidgetRenderer key={`${uid}-${index}`} widgetEndpoint={getEndpointUrl(resourceRefId, resourcesRefs)} />
       ))}
