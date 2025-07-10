e648102e385fd911fc4a828a82ae52211dd7403b
fedepini <fpini95@gmail.com>
2025-07-10 11:35:20 +0200

    refactor: updated Button to include actionHandler

--- src/utils/actionHandler.ts
+++ src/utils/actionHandler.ts
@@ -1,12 +1,20 @@
+import { useQueryClient } from '@tanstack/react-query'
+import useApp from 'antd/es/app/useApp'
 import { useNavigate } from 'react-router'
 
 import type { WidgetAction } from '../types/Widget'
 import { openDrawer } from '../widgets/Drawer/Drawer'
 import { openModal } from '../widgets/Modal/Modal'
 
-export const handleAction = async (action: WidgetAction, path: string) => {
+import { getAccessToken } from './getAccessToken'
+import type { RestApiResponse } from './types'
+import { getHeadersObject } from './utils'
+
+export const handleAction = async (action: WidgetAction, path: string, verb?: 'GET' | 'POST' | 'DELETE') => {
   const navigate = useNavigate()
-  const { requireConfirmation, type } = action
+  const queryClient = useQueryClient()
+  const { notification } = useApp()
+  const { id, requireConfirmation, type } = action
 
   switch (type) {
     case 'navigate':
@@ -29,5 +37,50 @@ export const handleAction = async (action: WidgetAction, path: string) => {
 
       break
     }
+    case 'rest': {
+      const { errorMessage, headers = [], onSuccessNavigateTo, payload, successMessage } = action
+
+      if (!requireConfirmation || window.confirm('Are you sure?')) {
+        if (verb === 'POST' && !payload) {
+          console.warn(`Payload not found for POST action ${id}`)
+        }
+
+        const res = await fetch(path, {
+          body: JSON.stringify(payload),
+          headers: {
+            ...getHeadersObject(headers),
+            Authorization: `Bearer ${getAccessToken()}`,
+          },
+          method: verb,
+        })
+
+        const json = (await res.json()) as RestApiResponse
+
+        if (!res.ok) {
+          notification.error({
+            description: errorMessage || json.message,
+            message: `${json.status} - ${json.reason}`,
+            placement: 'bottomLeft',
+          })
+          break
+        }
+
+        const actionName = verb === 'DELETE' ? 'deleted' : 'created'
+        notification.success({
+          description: successMessage || `Successfully ${actionName} ${json.metadata?.name} in ${json.metadata?.namespace}`,
+          message: json.message,
+          placement: 'bottomLeft',
+        })
+
+        await queryClient.invalidateQueries()
+
+        if (onSuccessNavigateTo) {
+          await navigate(onSuccessNavigateTo)
+        }
+      }
+
+      break
+    }
+    default: break
   }
 }
--- src/widgets/Button/Button.tsx
+++ src/widgets/Button/Button.tsx
@@ -1,16 +1,11 @@
 import type { IconProp } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useQueryClient } from '@tanstack/react-query'
 import { Button as AntdButton, Result } from 'antd'
-import useApp from 'antd/es/app/useApp'
-import { useNavigate } from 'react-router'
 
 import { useConfigContext } from '../../context/ConfigContext'
 import type { WidgetProps } from '../../types/Widget'
 import { handleAction } from '../../utils/actionHandler'
-import { getAccessToken } from '../../utils/getAccessToken'
-import type { RestApiResponse } from '../../utils/types'
-import { getHeadersObject, getResourceRef } from '../../utils/utils'
+import { getResourceRef } from '../../utils/utils'
 
 import styles from './Button.module.css'
 import type { Button as WidgetType } from './Button.type'
@@ -20,10 +15,7 @@ export type ButtonWidgetData = WidgetType['spec']['widgetData']
 const Button = ({ resourcesRefs, uid, widgetData }: WidgetProps<ButtonWidgetData>) => {
   const { actions, clickActionId, color, icon, label, shape, size, type } = widgetData
 
-  const navigate = useNavigate()
-  const queryClient = useQueryClient()
   const { config } = useConfigContext()
-  const { notification } = useApp()
 
   const action = Object.values(actions)
     .flat()
@@ -47,58 +39,11 @@ const Button = ({ resourcesRefs, uid, widgetData }: WidgetProps<ButtonWidgetData
     return null
   }
 
-  const { path, verb } = resourceRef
-
   const onClick = async () => {
-    if (action.type === 'rest') {
-      const { errorMessage, headers = [], id, onSuccessNavigateTo, payload, requireConfirmation, successMessage } = action
+    const { path } = resourceRef
 
-      if (requireConfirmation) {
-        const confirmed = window.confirm('Are you sure?')
-        if (!confirmed) {
-          return
-        }
-      }
-
-      const url = config?.api.SNOWPLOW_API_BASE_URL + path
-
-      if (verb === 'POST' && !payload) {
-        console.warn(`Payload not found for POST action ${id}`)
-      }
-
-      const res = await fetch(url, {
-        body: JSON.stringify(payload),
-        headers: {
-          ...getHeadersObject(headers),
-          Authorization: `Bearer ${getAccessToken()}`,
-        },
-        method: verb,
-      })
-
-      const json = (await res.json()) as RestApiResponse
-      if (!res.ok) {
-        notification.error({
-          description: errorMessage || json.message,
-          message: `${json.status} - ${json.reason}`,
-          placement: 'bottomLeft',
-        })
-        return
-      }
-
-      const actionName = verb === 'DELETE' ? 'deleted' : 'created'
-      notification.success({
-        description: successMessage || `Successfully ${actionName} ${json.metadata?.name} in ${json.metadata?.namespace}`,
-        message: json.message,
-        placement: 'bottomLeft',
-      })
-
-      await queryClient.invalidateQueries()
-      if (onSuccessNavigateTo) {
-        void navigate(onSuccessNavigateTo)
-      }
-    } else {
-      await handleAction(action, path)
-    }
+    const url = action.type === 'rest' ? config?.api.SNOWPLOW_API_BASE_URL + path : path
+    await handleAction(action, url)
   }
 
   const handleClick = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
