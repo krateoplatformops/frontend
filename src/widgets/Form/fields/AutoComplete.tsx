7eb8d030fa22fb0ed4d8f2e328ae29f8b17548f8
fedepini <fpini95@gmail.com>
2025-07-25 14:45:33 +0200

    feat: added loading inside Autocomplete

--- src/widgets/Form/fields/AutoComplete.tsx
+++ src/widgets/Form/fields/AutoComplete.tsx
@@ -1,29 +1,42 @@
-import { AutoComplete as AntAutoComplete, type AutoCompleteProps } from 'antd'
+import { LoadingOutlined } from '@ant-design/icons'
+import { AutoComplete as AntDAutoComplete, Spin, type AutoCompleteProps as AntDAutoCompleteProps } from 'antd'
+import useApp from 'antd/es/app/useApp'
 import debounce from 'lodash/debounce'
-import { useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 
 import useCatchError from '../../../hooks/useCatchError'
 
-const AutoComplete = ({ fetchOptions }: { fetchOptions: {
+interface AutoCompleteProps {
+  fetchOptions: {
     url: string
     verb: string
-  }}) => {
-  const [options, setOptions] = useState<AutoCompleteProps['options']>([])
-  const { catchError } = useCatchError()
+  }
+}
 
-  const getOptions = async (text: string) => {
+const AutoComplete = ({ fetchOptions }: AutoCompleteProps) => {
+  const [options, setOptions] = useState<AntDAutoCompleteProps['options']>([])
+  const { catchError } = useCatchError()
+  const { notification } = useApp()
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const getOptions = useCallback(async (text: string) => {
     if (!fetchOptions) {
       return
     }
+
+    const { url, verb } = fetchOptions
+
     try {
       let response: Response | undefined
       let data: unknown
-      if (fetchOptions.verb.toUpperCase() === 'GET') {
-        response = await fetch(`${fetchOptions.url}?q=${text}`)
+
+      if (verb.toUpperCase() === 'GET') {
+        response = await fetch(`${url}?q=${text}`)
         data = await response.json()
       }
-      if (fetchOptions.verb.toUpperCase() === 'POST') {
-        response = await fetch(fetchOptions.url, {
+
+      if (verb.toUpperCase() === 'POST') {
+        response = await fetch(url, {
           body: text,
           headers: {
             'Content-Type': 'application/json',
@@ -32,22 +45,42 @@ const AutoComplete = ({ fetchOptions }: { fetchOptions: {
         })
         data = await response.json()
       }
+
       if (Array.isArray(data)) {
         setOptions(data.map((item: string) => ({ value: item })))
       } else {
+        notification.error({
+          description: 'Invalid response format',
+          message: 'Error while retrieving options',
+          placement: 'bottomLeft',
+        })
         console.error('Invalid response format:', data)
       }
     } catch {
+      notification.error({
+        description: 'There has been an unhandled error while retrieving field options',
+        message: 'Error while retrieving options',
+        placement: 'bottomLeft',
+      })
       catchError({ message: 'Unable to retrieve field data' })
+    } finally {
+      setIsLoading(false)
     }
+  }, [catchError, fetchOptions, notification])
+
+  const debouncedGetOptions = useMemo(() => debounce((text: string) => getOptions(text), 1000), [getOptions])
+
+  useEffect(() => {
+    return () => { debouncedGetOptions.cancel() }
+  }, [debouncedGetOptions])
+
+  const handleSearch = (text: string) => {
+    setIsLoading(true)
+    void debouncedGetOptions(text)
   }
 
-  const handleSearch = debounce((text: string) => {
-    void getOptions(text)
-  }, 1000)
-
   return (
-    <AntAutoComplete
+    <AntDAutoComplete
       filterOption={(inputValue, option) => {
         if (option && typeof option.value === 'string') {
           return option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
@@ -56,6 +89,7 @@ const AutoComplete = ({ fetchOptions }: { fetchOptions: {
       }}
       onSearch={(text) => handleSearch(text)}
       options={options}
+      suffixIcon={isLoading ? <Spin indicator={<LoadingOutlined />} size='small' /> : null}
     />
   )
 }
