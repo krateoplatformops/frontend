296d485439d2f814e712883ac6b9f54cb3f9047e
fedepini <fpini95@gmail.com>
2025-09-19 17:20:28 +0200

    feat: added copy button

--- src/widgets/Markdown/Markdown.tsx
+++ src/widgets/Markdown/Markdown.tsx
@@ -1,3 +1,7 @@
+import { CopyOutlined } from '@ant-design/icons'
+import { Button } from 'antd'
+import { useState } from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard-ts'
 import { default as ReactMarkdown } from 'react-markdown'
 
 import type { WidgetProps } from '../../types/Widget'
@@ -10,8 +14,26 @@ export type MarkdownWidgetData = WidgetType['spec']['widgetData']
 const Markdown = ({ uid, widgetData }: WidgetProps<MarkdownWidgetData>) => {
   const { markdown } = widgetData
 
+  const [isCopied, setIsCopied] = useState(false)
+
   return (
     <div className={styles.markdown}>
+      <div className={styles.buttons}>
+        <div className={styles.button}>
+          {isCopied && 'Copied to clipboard'}
+
+          <CopyToClipboard
+            onCopy={() => {
+              setIsCopied(true)
+              setTimeout(() => setIsCopied(false), 2500)
+            }}
+            text={markdown}
+          >
+            <Button icon={<CopyOutlined />} size='large' />
+          </CopyToClipboard>
+        </div>
+      </div>
+
       <ReactMarkdown key={uid}>{markdown}</ReactMarkdown>
     </div>
   )
