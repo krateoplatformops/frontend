8e6ceead32d30376274fdb0f4d837c486edcb85b
fedepini <fpini95@gmail.com>
2026-01-29 16:55:57 +0100

    feat: updated css

--- src/theme/bridge.ts
+++ src/theme/bridge.ts
@@ -3,11 +3,16 @@ import type { GlobalToken } from 'antd'
 export const antdToCssVariables = (token: GlobalToken) => {
   const root = document.documentElement
 
+  // COLORS
+  root.style.setProperty('--gray-color', '#f5f5f5')
+
+  // THEME ELEMENTS
+  // Color of the application background
   root.style.setProperty('--background-color', token.colorBgLayout)
+  // Color of every border in the application
   root.style.setProperty('--border-color', token.colorBorder)
-  root.style.setProperty('--gray-color', token.colorTextSecondary)
-  root.style.setProperty('--light-color', token.colorBgContainer)
-  root.style.setProperty('--lightgray-color', token.colorFillSecondary)
-  root.style.setProperty('--panelbg-color', token.colorBgContainer)
+  // Color of the header background
+  root.style.setProperty('--header-color', token.colorBgContainer)
+  // Color of every primary element
   root.style.setProperty('--primary-color', token.colorPrimary)
 }
--- src/theme/defaultTheme.ts
+++ src/theme/defaultTheme.ts
@@ -5,10 +5,7 @@ export const defaultTheme: ThemeConfig = {
     colorBgContainer: '#FBFBFB',
     colorBgLayout: '#f5f5f5',
     colorBorder: '#E1E3E8',
-    colorFillSecondary: '#F0F0F0',
     colorPrimary: '#05629A',
-    colorTextSecondary: '#a0a0a0',
-    colorWhite: '#FFFFFF',
     fontFamily: 'Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif',
   },
 }
